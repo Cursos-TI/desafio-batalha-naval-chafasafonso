bfce9ab18093b39355b2714169418c866921a20b
chafasafonso <chagasafonso8@gmail.com>
2025-03-22 19:36:23 +0000

    batalha naval aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,41 +1,70 @@
 #include <stdio.h>
 
-int main() {
-    int tabuleiro[10][10];
-    int i, j;
+#define TAMANHO 10
+#define NAVIO 3
+#define AGUA 0
 
-    
-    for(i = 0; i < 10; i++){
-        for(j = 0; j < 10; j++){
-            tabuleiro[i][j] = 0;
-        }
+// tabuleiro 
+void imprimirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
+    printf("   ");
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d ", i);
     }
+    printf("\n");
 
-    // Tamanho fixo dos navios
-    int tamanho_navio = 3;
-
-    // Posicionamento do navio horizontal (linha 1, coluna 2)
-    int horiz_linha = 1;
-    int horiz_col = 2;
-    for(i = 0; i < tamanho_navio; i++){
-        tabuleiro[horiz_linha][horiz_col + i] = 3;
-    }
-
-    // Posicionamento do navio vertical (linha 5, coluna 7)
-    int vert_linha = 5;
-    int vert_col = 7;
-    for(i = 0; i < tamanho_navio; i++){
-        tabuleiro[vert_linha + i][vert_col] = 3;
-    }
-
-    // Exibe o tabuleiro
-    printf("Tabuleiro:\n");
-    for(i = 0; i < 10; i++){
-        for(j = 0; j < 10; j++){
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d  ", i);
+        for (int j = 0; j < TAMANHO; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
+}
+
+// Função para verificar se um navio pode ser colocado sem sobreposição
+int podePosicionar(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int direcao, int diagonal) {
+    for (int i = 0; i < 3; i++) {
+        int r = linha + (diagonal == 1 ? i : (diagonal == 2 ? i : (direcao == 1 ? i : 0)));
+        int c = coluna + (diagonal == 1 ? i : (diagonal == 2 ? -i : (direcao == 0 ? i : 0)));
+
+        if (r >= TAMANHO || c >= TAMANHO || r < 0 || c < 0 || tabuleiro[r][c] != AGUA) {
+            return 0; // Não pode posicionar
+        }
+    }
+    return 1; // Pode posicionar
+}
+
+// Função para posicionar um navio
+void posicionarNavio(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int direcao, int diagonal) {
+    for (int i = 0; i < 3; i++) {
+        int r = linha + (diagonal == 1 ? i : (diagonal == 2 ? i : (direcao == 1 ? i : 0)));
+        int c = coluna + (diagonal == 1 ? i : (diagonal == 2 ? -i : (direcao == 0 ? i : 0)));
+        tabuleiro[r][c] = NAVIO;
+    }
+}
+
+int main() {
+    int tabuleiro[TAMANHO][TAMANHO] = {0}; // Inicializa com água (0)
+
+    // Posicionando dois navios horizontais/verticais
+    if (podePosicionar(tabuleiro, 1, 2, 1, 0)) {
+        posicionarNavio(tabuleiro, 1, 2, 1, 0); // Vertical
+    }
+    if (podePosicionar(tabuleiro, 5, 4, 0, 0)) {
+        posicionarNavio(tabuleiro, 5, 4, 0, 0); // Horizontal
+    }
+
+    // Posicionando dois navios diagonais
+    if (podePosicionar(tabuleiro, 7, 2, 0, 1)) {
+        posicionarNavio(tabuleiro, 7, 2, 0, 1); // Diagonal principal "\"
+    }
+    if (podePosicionar(tabuleiro, 7, 7, 0, 2)) {
+        posicionarNavio(tabuleiro, 7, 7, 0, 2); // Diagonal secundária "/"
+    }
+
+    // Exibir tabuleiro
+    imprimirTabuleiro(tabuleiro);
 
     return 0;
 }
+
